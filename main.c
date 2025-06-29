58b3a7bd9a74a7e292f086d3694338e21e9c086d
Vwirvw <amirzidiaz6@gmail.com>
2025-06-29 19:54:42 +0330

    Init

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    char u;
+    int i;
+    puts("Enter character : \n");
+    u=getchar();
+    printf("Your Char is %c \n",u);
+    i=u;
+    printf("yor aski is %d",i);
+        return 0;
+}
